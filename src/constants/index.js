db2af53ca5a2482365eb0d4e28283286cacb16bc
Ubuntu <tushar-kloud@development-vm.unmxikdd3tvenixk2rbvaehyng.rx.internal.cloudapp.net>
2024-09-02 09:39:10 +0000

    replaced google doc link with google drive link for instruction manuals

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,7 +4,7 @@ export const labsDetails = [
       title: "Chain of Thought",
       description: "The Chain of Thought Scenarios Lab helps users master structured problem-solving by breaking down complex tasks into logical, manageable steps.",
       thumbnailImage: "https://img.freepik.com/free-vector/wireframe-robot-ai-artificial-intelligence-robotic-hand-machine-learning-cyber-mind-domination-concept_127544-854.jpg?t=st=1724666915~exp=1724670515~hmac=ab4ab609f283c5c0ca545ff7dc6f436599edcde3c204c95d8344bfe3d57720fc&w=900",
-      doc_url: "https://docs.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1SdUbjqNFRs42rP1lCnp7VqneuxRss09E&embedded=true",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=13dUj9eXxZgcCkmEV70jF12Gd5FRBfsil&embedded=true",
       file_upload: false,
       multiple_files: false
     },
@@ -13,7 +13,7 @@ export const labsDetails = [
       title: "Deterministic Inventory Models",
       description: "Explore how Large Language Models (LLMs) enhance business efficiency by transforming manual calculations into instant results, drastically cutting down processing time from minutes to seconds.",
       thumbnailImage: "https://img.freepik.com/free-photo/ai-cloud-concept-with-robot-arms_23-2149739749.jpg?t=st=1725183402~exp=1725187002~hmac=fcb69b9ff34819d7ff3b73f24eb5b9d9f0afb4c3b0da447aacd6f2040aa654bd&w=900",
-      doc_url: "https://docs.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1xcz-z_l4Gdfx-BoN9G5Eww0xI8KgbJue&embedded=true",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1udRVQOGUZSfOMcrEzOmvLjjfwXH3NyDm&embedded=true",
       file_upload: false,
       multiple_files: false
     },
@@ -22,7 +22,7 @@ export const labsDetails = [
       title: "Bank Customer Churn Prediction",
       description: "Designed to sharpen finance professionals' skills in prompt engineering, focusing on customer churn analysis and financial metrics to extract actionable insights using AI tools",
       thumbnailImage: "https://img.freepik.com/free-photo/ai-chip-artificial-intelligence-future-technology-innovation_53876-129780.jpg?t=st=1724667003~exp=1724670603~hmac=4eff56a176050fa82ebfb95a43da0c83b5d1f33095b177bec817d35e3af1532a&w=740",
-      doc_url: "https://docs.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1fS2hnzyCJcDChfeGX-k-ZgjNRfiygTFm&embedded=true",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1QSwCecHjMxIH5_fQ2dQPcuHmPRmYa2gj&embedded=true",
       file_upload: true,
       multiple_files: false
     },
@@ -31,7 +31,8 @@ export const labsDetails = [
       title: "A-Z Medicines",
       description: "Interacting with a dataset relevant to roles such as Pharmaceutical Product Managers, Supply Chain Managers, Marketing Analysts, and Data Scientists within the pharmaceutical industry.",
       thumbnailImage: "https://img.freepik.com/free-photo/ai-chip-intelligence-technology-deep-learning_53876-143122.jpg?t=st=1724685921~exp=1724689521~hmac=45d5c360cd42d326cdd34cb0e6d2a9ecb61babfdfa2befeb4e4710151b8e538a&w=740",
-      doc_url: "https://docs.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=113HStVDv2GXgx5BSZ4db-Ub-cRVUa09u&embedded=true",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=16DMaX7LEpuNYlLPx_uf4A7tCRQ6E8NFh&embedded=true",
+      // doc_url:"https://drive.google.com/drive/folders/14-mLmnFzej2j_WKt6zn7-HO6sKnKgDwY",
       file_upload: true,
       multiple_files: false
     },
@@ -40,7 +41,7 @@ export const labsDetails = [
       title: "Synthetic Data in Accounting",
       description: "The 'Synthetic Accounting Data' business lab offers practical experience in financial data analysis using a simulated dataset. It features two main sections for in-depth exploration of real-world financial scenarios.",
       thumbnailImage: "https://christinamueller.com/wp-content/uploads/2023/02/AI_artifical-intelligence-graphic-1024x1024.jpeg",
-      doc_url: "https://docs.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1K6vnjiHlU-J3VLmaWeiOxyy9VuHyyfAC&embedded=true",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=15Ibr7bgTEOHWA4_2at38B24ZZX7Y2KaQ&embedded=true",
       file_upload: true,
       multiple_files: true
     }
