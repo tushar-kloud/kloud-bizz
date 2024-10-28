d7c961ba33c500f38c2b8717c2a37e8d71f659e7
Ubuntu <tushar-kloud@development-vm.unmxikdd3tvenixk2rbvaehyng.rx.internal.cloudapp.net>
2024-10-28 09:22:16 +0000

    dev: added more labs and their assets

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,13 +1,4 @@
 export const labsDetails = [
-    // {
-    //   id: 252623,
-    //   title: "Chain of Thought",
-    //   description: "The Chain of Thought Scenarios Lab helps users master structured problem-solving by breaking down complex tasks into logical, manageable steps.",
-    //   thumbnailImage: "https://img.freepik.com/free-vector/wireframe-robot-ai-artificial-intelligence-robotic-hand-machine-learning-cyber-mind-domination-concept_127544-854.jpg?t=st=1724666915~exp=1724670515~hmac=ab4ab609f283c5c0ca545ff7dc6f436599edcde3c204c95d8344bfe3d57720fc&w=900",
-    //   doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=13dUj9eXxZgcCkmEV70jF12Gd5FRBfsil&embedded=true",
-    //   file_upload: false,
-    //   multiple_files: false
-    // },
     {
       id: 729192,
       title: "Deterministic Inventory Models",
@@ -17,31 +8,96 @@ export const labsDetails = [
       file_upload: false,
       multiple_files: false
     },
-    // {
-    //   id: 828192,
-    //   title: "Bank Customer Churn Prediction",
-    //   description: "Designed to sharpen finance professionals' skills in prompt engineering, focusing on customer churn analysis and financial metrics to extract actionable insights using AI tools",
-    //   thumbnailImage: "https://img.freepik.com/free-photo/ai-chip-artificial-intelligence-future-technology-innovation_53876-129780.jpg?t=st=1724667003~exp=1724670603~hmac=4eff56a176050fa82ebfb95a43da0c83b5d1f33095b177bec817d35e3af1532a&w=740",
-    //   doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1QSwCecHjMxIH5_fQ2dQPcuHmPRmYa2gj&embedded=true",
-    //   file_upload: true,
-    //   multiple_files: false
-    // },
-    // {
-    //   id: 839102,
-    //   title: "A-Z Medicines",
-    //   description: "Interacting with a dataset relevant to roles such as Pharmaceutical Product Managers, Supply Chain Managers, Marketing Analysts, and Data Scientists within the pharmaceutical industry.",
-    //   thumbnailImage: "https://img.freepik.com/free-photo/ai-chip-intelligence-technology-deep-learning_53876-143122.jpg?t=st=1724685921~exp=1724689521~hmac=45d5c360cd42d326cdd34cb0e6d2a9ecb61babfdfa2befeb4e4710151b8e538a&w=740",
-    //   doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=16DMaX7LEpuNYlLPx_uf4A7tCRQ6E8NFh&embedded=true",
-    //   // doc_url:"https://drive.google.com/drive/folders/14-mLmnFzej2j_WKt6zn7-HO6sKnKgDwY",
-    //   file_upload: true,
-    //   multiple_files: false
-    // },
     {
       id: 830201,
       title: "Financial Performance Analysis of a Listed Enterprise",
       description: "A focused analysis of JPMorgan Chase & Co.'s financial performance during the first and second quarters of 2024.",
       thumbnailImage: "https://christinamueller.com/wp-content/uploads/2023/02/AI_artifical-intelligence-graphic-1024x1024.jpeg",
       doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1jEMjcOOboqb5LEK9zJTLyiKEbm4TIyO9&embedded=true",
+      file_upload: true,
+      multiple_files: true
+    },
+
+    // NEW LABS
+    // https://drive.google.com/file/d/1WdgTSB1KSsbU8zmNj_xwJWpwypS52HIs/view?usp=sharing
+    {
+      id: 272718,
+      title: "Conversion Rate Optimization (CRO) Models",
+      description: "Delve into the strategies that maximize conversion rates, using data-driven techniques to transform site visitors into loyal customers effectively.",
+      thumbnailImage: "https://img.freepik.com/free-photo/ai-cloud-concept-with-robot-hand_23-2149739751.jpg?t=st=1730101768~exp=1730105368~hmac=5ad5bd1c80220cecb7dc8b7c0028bf1059ad6a20c8ce82936b52ed69fb55d32f&w=900",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1WdgTSB1KSsbU8zmNj_xwJWpwypS52HIs&embedded=true",
+      file_upload: true,
+      multiple_files: true
+    },
+    {
+      id: 849182,
+      title: "Auditing and Compliance",
+      description: "An in-depth examination of auditing practices and compliance standards, focusing on risk assessment and regulatory requirements to ensure financial integrity.",
+      thumbnailImage: "https://img.freepik.com/free-photo/ai-cloud-concept-with-lit-brain_23-2149739755.jpg?t=st=1730102723~exp=1730106323~hmac=43fe314f69d2ff6e750cf2fc2124957b175813bf2396dacaa4521fbf8e3ad01d&w=900",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1L7w9idFsECSdNWIIRlXIJb-A7-H910kL&embedded=true",
+      file_upload: true,
+      multiple_files: true
+    },
+    {
+      id: 412312,
+      title: "Customer Behavior Analysis",
+      description: "Explore the patterns of customer behavior to uncover insights that drive engagement and improve marketing strategies, fostering stronger brand loyalty.",
+      thumbnailImage: "https://img.freepik.com/free-photo/ai-cloud-concept-with-cube_23-2149739756.jpg?t=st=1730102486~exp=1730106086~hmac=702d1aed60e82aa2fa08a141aa72d1f1bbcdc91a76b03b3bc14c40274a1338c1&w=900",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=16NiGLJLvPPA3E-sNJ5bxNSJt4Qmd4-ay&embedded=true",
+      file_upload: true,
+      multiple_files: true
+    },
+    {
+      id: 492812,
+      title: "Customer Churn Analysis",
+      description: "Utilize analytical tools to identify key factors leading to customer churn, enabling proactive measures to enhance retention and satisfaction.",
+      thumbnailImage: "https://img.freepik.com/free-photo/gradient-ai-cloud-with-broken-pieces_23-2149739757.jpg?t=st=1730102512~exp=1730106112~hmac=2de3fa800e635f50d3c52525b08dace619bed90fe92ca58a71f4d5770d6309d0&w=900",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1hNr3k_CHz5SfEaMgACAtTy-LlO9XoYsf&embedded=true",
+      file_upload: true,
+      multiple_files: true
+    },
+    {
+      id: 838572,
+      title: "Digital Marketing Spend ROI Analysis",
+      description: "Analyze the effectiveness of digital marketing expenditures by measuring return on investment, leveraging insights to optimize budget allocations and enhance campaign strategies.",
+      thumbnailImage: "https://img.freepik.com/free-photo/ai-cloud-with-robot-head_23-2149739759.jpg?t=st=1730102552~exp=1730106152~hmac=81e0bbbc35a19f58460730e2d8475e51d05d3ce949d685b57a8ae77175a7154b&w=900",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1NuXZbgVjG4aTbo-v9xtbCURFiU9tnbgd&embedded=true",
+      file_upload: true,
+      multiple_files: true
+    },
+    {
+      id: 898699,
+      title: "Marketing By Numbers",
+      description: "Discover the quantitative approach to marketing strategies, utilizing analytics to drive decisions and enhance campaign effectiveness.",
+      thumbnailImage: "https://img.freepik.com/free-photo/ai-cloud-concept-with-robot-face_23-2149739753.jpg?t=st=1730102578~exp=1730106178~hmac=7ec131e4ec5aa25eea78235bf9f4315608adb0b3d89365e333cf5964756e81a3&w=900",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1MCBKcR_oWLAtWCmKhaomIcn6VL8k6LtS&embedded=true",
+      file_upload: true,
+      multiple_files: true
+    },
+    {
+      id: 629173,
+      title: "Profile Margin Analysis of Ecommerce",
+      description: "Examine the margins across product profiles to identify profitability patterns, enabling better pricing strategies and inventory management.",
+      thumbnailImage: "https://img.freepik.com/premium-photo/ai-cloud-concept-with-gradient-cube_23-2149739746.jpg?w=900",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1wfJFphgYpqsre-_scCVttGeggluWzS0r&embedded=true",
+      file_upload: true,
+      multiple_files: true
+    },
+    {
+      id: 869283,
+      title: "Shipping Cost Optimization for Ecommerce",
+      description: "Evaluate shipping strategies to reduce costs while maintaining service quality, optimizing logistics for enhanced customer satisfaction.",
+      thumbnailImage: "https://img.freepik.com/free-photo/ai-cloud-concept-with-robot-arms_23-2149739749.jpg?t=st=1725183402~exp=1725187002~hmac=fcb69b9ff34819d7ff3b73f24eb5b9d9f0afb4c3b0da447aacd6f2040aa654bd&w=900",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1l_gxtNEx_QjKlKsKKE8ipCn0eBTdhKfV&embedded=true",
+      file_upload: true,
+      multiple_files: true
+    },
+    {
+      id: 838572,
+      title: "Digital Marketing Spend ROI Analysis",
+      description: "Analyze the effectiveness of digital marketing expenditures by measuring return on investment, leveraging insights to optimize budget allocations and enhance campaign strategies.",
+      thumbnailImage: "https://img.freepik.com/premium-photo/ai-cloud-concept-with-brain_23-2149739747.jpg?w=900",
+      doc_url: "https://drive.google.com/gview?a=v&pid=explorer&chrome=false&api=true&embedded=true&srcid=1NuXZbgVjG4aTbo-v9xtbCURFiU9tnbgd&embedded=true",
       file_upload: true,
       multiple_files: true
     }
